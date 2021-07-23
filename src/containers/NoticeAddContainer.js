0a978c396d26b70dd66abaf53accb5c9213b5dac
circulusjmkim <jmkim@circul.us>
2021-07-23 11:04:37 +0900

    공지사항(markdown) 등록 에디터, 미리보기 추가

--- src/containers/NoticeAddContainer.js
+++ src/containers/NoticeAddContainer.js
@@ -1,5 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMde from "react-mde";
+import * as Showdown from "showdown";
+import "../styles/mdeStyle.css";
 
-const NoticeAddContainer = () => <div>NoticeAddContainer</div>;
+/* const loadSuggestions = (text) => new Promise((accept) => {
+    setTimeout(() => {
+      const suggestions = [
+        {
+          preview: "Andre",
+          value: "@andre"
+        },
+        {
+          preview: "Angela",
+          value: "@angela"
+        },
+        {
+          preview: "David",
+          value: "@david"
+        },
+        {
+          preview: "Louise",
+          value: "@louise"
+        }
+      ].filter((i) => i.preview.toLowerCase().includes(text.toLowerCase()));
+      accept(suggestions);
+    }, 250);
+  }); */
+
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true
+});
+
+const NoticeAddContainer = () => {
+  const [value, setValue] = useState("**Hello world!!!**");
+  const [selectedTab, setSelectedTab] = useState("write");
+
+  return (
+    <div className="container">
+      <ReactMde
+        value={value}
+        onChange={setValue}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+        // loadSuggestions={loadSuggestions}
+        childProps={{
+          writeButton: {
+            tabIndex: -1
+          }
+        }}
+      />
+    </div>
+  );
+}
 
 export default NoticeAddContainer;
+
