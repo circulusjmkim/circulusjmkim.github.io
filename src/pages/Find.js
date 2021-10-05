48fde258b8f85e603a4c6656dbf5d9b789485a4e
circulusjmkim <jmkim@circul.us>
2021-10-05 14:21:52 +0900

    단일 메뉴일 때도 메뉴 설정하도록 수정

--- src/pages/Find.js
+++ src/pages/Find.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { useLocation } from 'react-use';
 import FindUserContainer from '../containers/FindUserContainer';
+import { MENUS } from '../core/utils/consts';
 
 const Find = () => {
+  const history = useHistory();
+  const { pathname } = useLocation();
   const { item } = useSelector(state => state.page);
+  useEffect(() => {
+    let index = 0;
+    if(item >= 0) {
+      index = item;
+    }
+    const menu = MENUS[0].list[index].path;
+    history.replace(`${pathname}?menu=${menu}`);
+  }, [item]);
 
   return (<>
     {item === 0 && <FindUserContainer /> }
