df21d2ee2cd3f923822583c786c95b92e21a3f9c
circulusjmkim <jmkim@circul.us>
2022-07-21 10:16:09 +0900

    회원가입 시 전화번호 입력에 숫자만 들어가도록 수정

--- src/features/signup.js
+++ src/features/signup.js
@@ -1,20 +1,27 @@
 /* eslint-disable no-empty-pattern */
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import moment from "moment";
-import { checkUserId, checkUserInfo, userSignUp } from "../api/amapi";
-import { validateBirthDate, validateEmail, validateId, validateName, validatePassword, validateTel } from "../core/utils/validate";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import moment from 'moment';
+import { checkUserId, checkUserInfo, userSignUp } from '../api/amapi';
+import {
+  validateBirthDate,
+  validateEmail,
+  validateId,
+  validateName,
+  validatePassword,
+  validateTel,
+} from '../core/utils/validate';
 
 const getErrorObj = (prop, msg, error) => {
   let rejectValue;
-  if(error && typeof error === 'object') {
-    rejectValue = {...error, [prop]: msg};
+  if (error && typeof error === 'object') {
+    rejectValue = { ...error, [prop]: msg };
   } else if (msg) {
-    rejectValue = {[prop]: msg};
+    rejectValue = { [prop]: msg };
   } else if (!msg) {
     rejectValue = error;
   }
   return rejectValue;
-}
+};
 
 export const checkId = createAsyncThunk(
   'signup/CHECK_ID',
@@ -24,20 +31,24 @@ export const checkId = createAsyncThunk(
       const { error } = signup;
       const { result, error: e } = await checkUserId(userId);
       let rejectValue = getErrorObj('userId', '', error);
-      if(result) {
+      if (result) {
         return { idChecked: result, error: rejectValue };
       }
 
       let msg;
-      if(e && 'code' in e && 'desc' in e) {
+      if (e && 'code' in e && 'desc' in e) {
         msg = `🙅🏻‍♀️ ${error.desc} 🙅🏻‍♂️`;
       }
       msg = '이미 존재하는 아이디입니다.';
-      
+
       rejectValue = getErrorObj('userId', msg, error);
       return rejectWithValue(rejectValue);
     } catch (error) {
-      const rejectValue = getErrorObj('userId', `${JSON.stringify(error)}:😥 아이디 중복 체크 중 오류 발생`, error);
+      const rejectValue = getErrorObj(
+        'userId',
+        `${JSON.stringify(error)}:😥 아이디 중복 체크 중 오류 발생`,
+        error,
+      );
       return rejectWithValue(rejectValue);
     }
   },
@@ -45,28 +56,38 @@ export const checkId = createAsyncThunk(
 
 export const checkInfo = createAsyncThunk(
   'signup/CHECK_INFO',
-  async ({name: target, value}, { rejectWithValue, getState }) => {
+  async ({ name: target, value }, { rejectWithValue, getState }) => {
     try {
       const { signup } = getState();
-      const { error, data: {verified} } = signup;
-      const { result, error: e } = await checkUserInfo({target, value});
+      const {
+        error,
+        data: { verified },
+      } = signup;
+      const { result, error: e } = await checkUserInfo({ target, value });
       let rejectValue = getErrorObj(target, '', error);
-      if(result) {
-        return { verified: {...verified, [target]: result}, error: rejectValue };
+      if (result) {
+        return {
+          verified: { ...verified, [target]: result },
+          error: rejectValue,
+        };
       }
       let msg;
-      if(e && 'code' in e && 'desc' in e) {
+      if (e && 'code' in e && 'desc' in e) {
         msg = `🙅🏻‍♀️ ${error.desc} 🙅🏻‍♂️`;
       }
       msg = `이미 존재하는 ${target} 인증정보 입니다.`;
-      
+
       rejectValue = getErrorObj(target, msg, error);
       return rejectWithValue(rejectValue);
     } catch (error) {
-      const rejectValue = getErrorObj(target, `${JSON.stringify(error)}:😥 인증 정보 중복 체크 중 오류 발생`, error);
+      const rejectValue = getErrorObj(
+        target,
+        `${JSON.stringify(error)}:😥 인증 정보 중복 체크 중 오류 발생`,
+        error,
+      );
       return rejectWithValue(rejectValue);
     }
-  }
+  },
 );
 
 export const signUp = createAsyncThunk(
@@ -76,15 +97,17 @@ export const signUp = createAsyncThunk(
       const { signup } = getState();
       const { data } = signup;
       const { result, error } = await userSignUp(data);
-      if(result) {
+      if (result) {
         return result;
       }
-      if(error && 'code' in error && 'desc' in error) {
-        return rejectWithValue(`🙅🏻‍♀️ ${error.desc} 🙅🏻‍♂️`)
+      if (error && 'code' in error && 'desc' in error) {
+        return rejectWithValue(`🙅🏻‍♀️ ${error.desc} 🙅🏻‍♂️`);
       }
       return rejectWithValue(`😥 회원가입 실패`);
     } catch (error) {
-      return rejectWithValue(`${JSON.stringify(error)}:😥 회원가입 중 오류 발생`);
+      return rejectWithValue(
+        `${JSON.stringify(error)}:😥 회원가입 중 오류 발생`,
+      );
     }
   },
 );
@@ -92,7 +115,17 @@ export const signUp = createAsyncThunk(
 const initialState = {
   loading: false,
   bManager: false,
-  data: { userId: '', password: '', lastName: '', firstName: '', email: '', tel: '', birthDate: moment().format('YYYY-MM-DD'), verified: {email: false, tel: false}, role: '0000'},
+  data: {
+    userId: '',
+    password: '',
+    lastName: '',
+    firstName: '',
+    email: '',
+    tel: '',
+    birthDate: moment().format('YYYY-MM-DD'),
+    verified: { email: false, tel: false },
+    role: '0000',
+  },
   idChecked: false,
   error: null,
   saved: null,
@@ -102,15 +135,26 @@ const signupSlice = createSlice({
   name: 'signup',
   initialState,
   reducers: {
-    initialize: () => ({...initialState, }),
-    setToggle: (state) => ({...state, bManager: !state.bManager, data: {...state.data, role: !state.bManager?'0110':'0000'}}),
-    setParams: (state, action) => ({...state, data: {...state.data, ...action.payload}, }),
-    setClear: (state, action) => ({...initialState, menu: state.menu, data: action.payload, }),
-    setError: (state, action) => ({...state, error: action.payload}),
-    initChecked: (state) => ({...state, idChecked: !state.idChecked}),
+    initialize: () => ({ ...initialState }),
+    setToggle: (state) => ({
+      ...state,
+      bManager: !state.bManager,
+      data: { ...state.data, role: !state.bManager ? '0110' : '0000' },
+    }),
+    setParams: (state, action) => ({
+      ...state,
+      data: { ...state.data, ...action.payload },
+    }),
+    setClear: (state, action) => ({
+      ...initialState,
+      menu: state.menu,
+      data: action.payload,
+    }),
+    setError: (state, action) => ({ ...state, error: action.payload }),
+    initChecked: (state) => ({ ...state, idChecked: !state.idChecked }),
   },
   extraReducers: {
-    [checkId.pending.type]: state => ({ ...state, loading: true,}),
+    [checkId.pending.type]: (state) => ({ ...state, loading: true }),
     [checkId.fulfilled.type]: (state, action) => ({
       ...state,
       loading: false,
@@ -123,12 +167,12 @@ const signupSlice = createSlice({
       error: action.payload,
       idChecked: false,
     }),
-    [checkInfo.pending.type]: state => ({ ...state, loading: true,}),
+    [checkInfo.pending.type]: (state) => ({ ...state, loading: true }),
     [checkInfo.fulfilled.type]: (state, action) => ({
       ...state,
       loading: false,
       error: action.payload.error,
-      data: {...state.data, verified: action.payload.verified},
+      data: { ...state.data, verified: action.payload.verified },
     }),
     [checkInfo.rejected.type]: (state, action) => ({
       ...state,
@@ -136,7 +180,7 @@ const signupSlice = createSlice({
       error: action.payload,
       idChecked: false,
     }),
-    [signUp.pending.type]: state => ({ ...state, loading: true}),
+    [signUp.pending.type]: (state) => ({ ...state, loading: true }),
     [signUp.fulfilled.type]: (state, action) => ({
       ...state,
       loading: false,
@@ -149,48 +193,71 @@ const signupSlice = createSlice({
       error: action.payload,
       saved: false,
     }),
-  }
+  },
 });
 
 const { reducer: signupReducer, actions } = signupSlice;
-export const { initialize, setMenu, setParams, setClear, setError, setToggle, initChecked } = actions;
+export const {
+  initialize,
+  setMenu,
+  setParams,
+  setClear,
+  setError,
+  setToggle,
+  initChecked,
+} = actions;
 
-const getValidateResult = ({value, name}) =>
-{ 
+const getValidateResult = ({ value, name }) => {
   switch (name) {
-    case 'userId': return validateId(value);
-    case 'password': return validatePassword(value);
-    case 'lastName': return validateName(value, name);
-    case 'firstName': return validateName(value, name);
-    case 'email': return validateEmail({value});
-    case 'tel': return validateTel({value});
-    case 'birthDate': return validateBirthDate(value);
-    default: return '';
+    case 'userId':
+      return validateId(value);
+    case 'password':
+      return validatePassword(value);
+    case 'lastName':
+      return validateName(value, name);
+    case 'firstName':
+      return validateName(value, name);
+    case 'email':
+      return validateEmail({ value });
+    case 'tel':
+      return validateTel({ value });
+    case 'birthDate':
+      return validateBirthDate(value);
+    default:
+      return '';
   }
-}
+};
 
 export const textChange = (e) => (dispatch, getState) => {
-  const { target: { value, name } } = e;
-  const msg = getValidateResult({name, value});
+  const {
+    target: { value, name },
+  } = e;
+  const msg = getValidateResult({ name, value });
   const { signup } = getState();
   const { error, idChecked } = signup;
-  dispatch(setError({...error, [name]: msg}));
-  dispatch(setParams({ [name]: value }));
-  if(idChecked && name === 'userId') {
+  dispatch(setError({ ...error, [name]: msg }));
+  if (name === 'tel') {
+    dispatch(setParams({ [name]: value.replace(/[^\d]/g, '') }));
+  } else {
+    dispatch(setParams({ [name]: value }));
+  }
+  if (idChecked && name === 'userId') {
     dispatch(initChecked());
   }
-}
+};
 
 export const clearClick = (e, bUser) => (dispatch) => {
-  const { target: { name: n } } = e;
+  const {
+    target: { name: n },
+  } = e;
   let name = bUser ? 'user' : 'robot';
-  if(n === 'logical') {
+  if (n === 'logical') {
     name += 'Id';
   }
-  if(n === 'physical') {
+  if (n === 'physical') {
     name += 'PId';
   }
   dispatch(setParams({ [name]: '' }));
-}
+};
 
 export default signupReducer;
